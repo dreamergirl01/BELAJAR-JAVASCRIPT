ab41f7aae12b6826ffceb3b52c60c0daa9a05363
dreamergirl01 <rahma01twins@gmail.com>
2025-02-24 20:46:14 +0700

    dom manipulation

---
+++ domManipulation/domManipulation.js
@@ -0,0 +1,7 @@
+const header = document.getElementById('judul')
+header.innerHTML = '<em>Rahma</em>'
+header.style.color = 'lightblue'
+header.style.backgroundColor = 'salmon'
+
+//menambahkan atribut class pada sebuah element (tambahkan label pada p2)
+const p2 = document.querySelector('.p2')
